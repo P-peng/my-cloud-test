8cba733465b97e13e0b40129ef9223222fcecd1e
P-peng <894194651@qq.com>
2020-05-18 17:48:45 +0800

    first commit

---
+++ nacos/src/main/java/com/ge/Nacos/NacosApplication.java
@@ -0,0 +1,33 @@
+package com.ge.Nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dengzhipeng
+ * @version 1.0
+ * @date 2020/5/18 0018
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return string;
+        }
+    }
+
+}
