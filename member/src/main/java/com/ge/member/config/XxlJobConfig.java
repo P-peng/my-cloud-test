b14c143f79ec88109bc15982e0c7b6648b597a45
P-peng <894194651@qq.com>
2020-05-20 14:54:14 +0800

    xxl-job

---
+++ member/src/main/java/com/ge/member/config/XxlJobConfig.java
@@ -0,0 +1,58 @@
+package com.ge.member.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 初始化 xxl-job 任务调度器
+ * @author dengzhipeng
+ * @version 1.0
+ * @date 2020/5/20 0020
+ */
+@Configuration
+public class XxlJobConfig {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appName;
+
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+    /**
+     * xxl-job 初始化
+     */
+//    @Bean
+//    public XxlJobSpringExecutor xxlJobExecutor() {
+//        logger.debug(">>>>>>>>>>> xxl-job config init.");
+//        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+//        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+//        xxlJobSpringExecutor.setAppName(appName);
+//        xxlJobSpringExecutor.setIp(ip);
+//        xxlJobSpringExecutor.setPort(port);
+//        xxlJobSpringExecutor.setAccessToken(accessToken);
+//        xxlJobSpringExecutor.setLogPath(logPath);
+//        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+//        return xxlJobSpringExecutor;
+//    }
+}
--- member/src/main/java/com/ge/member/job/DemoJobHandler.java
+++ member/src/main/java/com/ge/member/job/DemoJobHandler.java
@@ -2,7 +2,7 @@ package com.ge.member.job;
 
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
-import com.xxl.job.core.handler.annotation.JobHandler;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import com.xxl.job.core.log.XxlJobLogger;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Component;
  * @version 1.0
  * @date 2020/5/19 0019
  */
-@JobHandler(value = "demoJobHandler")
 @Component
 public class DemoJobHandler extends IJobHandler {
 
+    @XxlJob("demoJobHandler")
     @Override
     public ReturnT<String> execute(String param) {
         XxlJobLogger.log("XXL-JOB, Hello World.");
